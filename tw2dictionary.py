648ff940276cc7bc35845427e8265cc1615e9e02
dontdisplay <113240108+dontdisplay@users.noreply.github.com>
2022-09-23 10:59:26 +0530

    Update tw2dictionary.py

--- tw2dictionary.py
+++ tw2dictionary.py
@@ -1,44 +1,46 @@
-def addinfo(courses):
-    c_code=input("Enter the course code").upper()
-    if c_code not in courses:
-        c_name=input("Enter the name of the course")
+def addcourse(courses):
+    course=input("Enter the course id").upper()
+    if course not in courses:
+        c_name=input("Enter the course name")
         c_faculty=input("Enter the name of faculty")
         c_reg=int(input("Enter the number of registrations"))
-        courses[c_code]={'course_name':c_name,'faculty':c_faculty,'no_of_reg':c_reg}
+        courses[course]={'course_name':c_name,'faculty':c_faculty,'no_of_reg':c_reg}
     else:
-        print("duplicate course entry")
+        print("Duplicate course entry!! Entry denied")
+
 
 def display(courses):
     for course in courses:
-        print("----------------------------------------------------------------------")
-        print("THE DETAILS OF COURSE-",course)
-        print("coursename-",courses[course]['course_name'])
-        print("faculty-",courses[course]['faculty'])
-        print("no of reg-",courses[course]['no_of_reg'])
-
+        print("--------------------------------------------------------------------------------\n\n")
+        print("Details of the course -",course)
+        print("Name->",courses[course]['course_name'])
+        print("Faculty->",courses[course]['faculty'])
+        print("NO_OF_registrations->",courses[course]['no_of_reg'])
 
 def highestreg(courses):
     cr=0
-    c_name=""
+    cname=""
     for course in courses:
         if courses[course]['no_of_reg']>cr:
             cr=courses[course]['no_of_reg']
-            c_name=courses[course]['course_name']
-    print("the courese with highest registrations is")
-    print("course-",c_name,"no of reg-",cr)
-
+            cname=courses[course]['course_name']
+    print("-----------------------------------------------------------------------------------\n\n")
+    print("The Highest Registered course is",cname," with ",cr," registrations")
 
 
 def main():
     courses={}
-    while True:
-        print("Enter 1 to add information of courses")
-        print("Enter 2 to display infromation of courses")
-        print("Enter 3 to find the course with highest registrations")
-        ch=int(input("Enter your choice"))
-        if ch==1:
-            addinfo(courses)
 
+    while True:
+        print("---------------------------------------------------------------------------------------------\n")
+        print("Enter 1 for adding a course")
+        print("Enter 2 for displaying present courses")
+        print("Enter 3 for finding highest registerd course")
+        print("Enter 4 to exit")
+        ch=int(input("Enter you choice"))
+
+        if ch==1:
+            addcourse(courses)
         if ch==2:
             display(courses)
         if ch==3:
